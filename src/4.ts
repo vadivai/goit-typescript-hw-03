3792d69da4ed8d911a0e8cc48314978bb73e45cc
vadivai <vaikanvn@gmail.com>
2024-01-02 22:59:39 +0200

    Add 4

--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,66 @@
-const key = new Key();
+// У цьому завдання вам належить реалізувати сценарій життя, де людина, ключ і будинок взаємодіють один з одним.
+// Ключ (Key): Створіть клас Key. У нього має бути одна приватна властивість signature,
+// яка генерується випадково при створенні об'єкта цього класу (наприклад Math.random()).
+// Також цей клас повинен мати метод getSignature, який повертає значення властивості signature.
 
-const house = new MyHouse(key);
-const person = new Person(key);
+// Людина (Person): Створіть клас Person. Конструктор цього класу приймає об'єкт класу Key і зберігає їх у приватному властивості key.
+// Клас Person повинен мати метод getKey, який повертає збережений ключ.
 
-house.openDoor(person.getKey());
+// Дім (House): Створіть абстрактний клас House. Цей клас має дві властивості: door, яка може бути відкрита (true), або закрита (false),
+// і key, яка зберігає об'єкт класу Key. У цьому класі також повинен бути метод comeIn, який додає об'єкт класу Person у масив tenants, якщо door відкрита.
+// Ваш абстрактний клас House також повинен мати абстрактний метод OpenDoor, який приймає об'єкт класу Key.
 
-house.comeIn(person);
+// Мій будинок (MyHouse): Створіть клас MyHouse, який успадковується від абстрактного класу House.
+// Реалізуйте метод openDoor у цьому класі.Якщо ключ, переданий цьому методу, збігається з ключем, збереженим як key, то двері відчиняються.
 
+// Після реалізації всіх класів створіть об'єкти для кожного класу та спробуйте відтворити сценарій, в якому людина приходить додому.
 
-export {};
+class Key {
+  constructor(private signature: number) {
+    this.signature = Math.random();
+  }
+  getSignature() {
+    return this.signature;
+  }
+}
+
+class Person {
+  constructor(private key: Key) {
+    this.key = key;
+  }
+  getKey() {
+    return this.key;
+  }
+}
+
+abstract class House {
+  private door: boolean = false;
+  private tenants: Person[] = [];
+  constructor(private key: Key) {}
+
+  comeIn(tenant: Person) {
+    if (this.door) {
+      this.tenants.push(tenant);
+    }
+  }
+  abstract openDoor(key: Key) {}
+}
+
+class MyHouse extends House {
+  openDoor(key: Key) {
+    if (key.getSignature() === this.key.getSignature) {
+      this.door = true;
+    }
+  }
+}
+
+// const key = new Key();
+
+// const house = new MyHouse(key);
+// const person = new Person(key);
+
+// house.openDoor(person.getKey());
+
+// house.comeIn(person);
+
+// export {};
